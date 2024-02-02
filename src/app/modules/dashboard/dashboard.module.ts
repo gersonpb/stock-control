2a02b5202d99d4cfa789ca2785d6cb1c5912bae1
gersonpb <gersonpb@gmail.com>
2024-02-02 19:44:49 -0300

    implementando o controle de estoque

---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ChartModule } from 'primeng/chart';
+import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+
+
+import { DASHBOARD_ROUTES } from './dashboard.rounting';
+import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [DashboardHomeComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(DASHBOARD_ROUTES),
+    //Primeng
+    SidebarModule,
+    ButtonModule,
+    ToolbarModule,
+    CardModule,
+    ToastModule,
+    ChartModule,
+    //shared
+    SharedModule,
+  ],
+  providers: [MessageService, CookieService],
+})
+export class DashboardModule { }
---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { environment } from 'src/app/environments/environment';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
+import { AuthResponse } from 'src/app/models/interfaces/user/auth/AutResponse';
+import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient, private cookie: CookieService) {}
+
+  signupUser(requestDatas: SignupUserRequest): Observable<SignupUserResponse> {
+    return this.http.post<SignupUserResponse>(
+      `${this.API_URL}/user`,
+      requestDatas
+    );
+  }
+
+  authUser(requestDatas: AuthRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(
+      `${this.API_URL}/auth`,
+      requestDatas
+    );
+  }
+
+  isLoggedIn(): boolean {
+    const JWT_TOKEN = this.cookie.get('USER_INFO');
+    return JWT_TOKEN ? true : false;
+  }
+
+}
